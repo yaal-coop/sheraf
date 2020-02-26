780bd977e669016bc780e04896b37ca2d3662077
Éloi Rivard <eloi@yaal.fr>
2020-02-26 11:17:55 +0100

    Fixed last commit

--- tests/attributes/test_base.py
+++ tests/attributes/test_base.py
@@ -1,26 +1,26 @@
 import sheraf
 
 
-def test_default_read_and_write_memoization_attribute(self, sheraf_connection):
+def test_default_read_and_write_memoization_attribute(sheraf_connection):
     attribute = sheraf.BaseAttribute()
     assert attribute.read_memoization is False
     assert attribute.write_memoization is True
 
 
-def test_read_and_write_memoization_parameters(self, sheraf_connection):
+def test_read_and_write_memoization_parameters(sheraf_connection):
     attribute = sheraf.BaseAttribute(read_memoization=True, write_memoization=False)
     assert attribute.read_memoization is True
     assert attribute.write_memoization is False
 
 
-def test_set_read_memoization_to_true(self, sheraf_connection):
+def test_set_read_memoization_to_true(sheraf_connection):
     sheraf.attributes.base.set_read_memoization(True)
     attribute = sheraf.BaseAttribute()
     assert attribute.read_memoization is True
     sheraf.attributes.base.set_read_memoization(False)
 
 
-def test_set_write_memoization_to_false(self, sheraf_connection):
+def test_set_write_memoization_to_false(sheraf_connection):
     sheraf.attributes.base.set_write_memoization(False)
     attribute = sheraf.BaseAttribute()
     assert attribute.write_memoization is False
