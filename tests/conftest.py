47a8a532e8c3ace92d93001c771957ef7889b4e1
Éloi Rivard <eloi@yaal.fr>
2020-03-04 16:05:09 +0100

    We do not use the postgresql fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,7 @@ from .utils import (
     create_database,
     create_temp_directory,
     delete_temp_directory,
-    start_postgresql_server,
     start_zeo_server,
-    stop_postgresql_server,
     stop_zeo_server,
 )
 
@@ -142,44 +140,6 @@ def sheraf_zeo_database(request, sheraf_zeo_server):
                 *sheraf_zeo_server
             )
         },
-    )
-
-    database = None
-    try:
-        database = create_database(db_kwargs)
-        yield database
-
-    finally:
-        if database:
-            with sheraf.databases.connection(commit=True) as conn:
-                for key in list(conn.root().keys()):
-                    del conn.root()[key]
-        close_database(database)
-
-
-@pytest.fixture(scope="session")
-def sheraf_pgsql_server():
-    """This fixture launches a PostgreSQL server, if available."""
-
-    temp_directory, old_files_root_dir = create_temp_directory()
-    pg_process, pg_user, pg_port, pg_error = start_postgresql_server(temp_directory)
-    if pg_error:
-        pytest.skip(pg_error)
-    yield pg_user, pg_port
-    stop_postgresql_server(pg_process)
-    delete_temp_directory(temp_directory, old_files_root_dir)
-
-
-@pytest.fixture
-def sheraf_pgsql_relstorage_database(request, sheraf_pgsql_server):
-    """This fixture launches a PostgreSQL server, and then creates a database
-    using a :class:`~relstorage.storage.RelStorage`.
-
-    .. note :: Only one PostgreSQL server is launched by test session.
-    """
-
-    db_kwargs = load_database_kwargs(
-        request, {"uri": "postgres://{}@localhost:{}/zodb".format(*sheraf_pgsql_server)}
     )
 
     database = None
