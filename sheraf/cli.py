52538108c871c91bad7736e5bcb729b2697e79e2
Éloi Rivard <azmeuk@gmail.com>
2021-02-01 23:02:49 +0100

    Sheraf binary. Fixes #20

---
+++ sheraf/cli.py
@@ -0,0 +1,23 @@
+import click
+import sheraf
+import sys
+from rich.traceback import install
+
+
+install(show_locals=True)
+
+
+@click.group()
+@click.argument("uri")
+def cli(uri):
+    sys.path.append(".")
+    db = sheraf.Database(uri)
+    yield
+    db.close()
+
+
+@cli.command()
+@click.argument("models", nargs=-1)
+def check(models):
+    with sheraf.connection():
+        sheraf.print_health(*models)
