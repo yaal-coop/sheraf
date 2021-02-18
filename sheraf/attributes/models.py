62a1377dab35e23b10e10a36414cd27d205cb41e
Éloi Rivard <azmeuk@gmail.com>
2021-02-18 19:50:42 +0100

    Minor ModelAttribute refactoring

--- sheraf/attributes/models.py
+++ sheraf/attributes/models.py
@@ -1,7 +1,81 @@
 import sheraf
 from sheraf.models.indexation import model_from_table
-from sheraf.attributes import ModelLoader
 from sheraf.attributes.base import BaseAttribute
+
+
+class ModelLoader(object):
+    """
+    Loads models from the base in a cache.
+
+    Inherited by most model types
+    (``Model[Attribute|List|Set|(Large)Dict]``)
+    """
+
+    cache = {}
+
+    def __init__(self, model=None, **kwargs):
+        super().__init__(**kwargs)
+        self._model = model
+
+    def load_model(self, modelpath):
+        # Internal.
+        # :param modelpath: namespace of the model to load (eg 'x.y.z')
+        # :return: a model class given its path
+        if isinstance(modelpath, bytes):
+            modelpath = modelpath.decode("utf-8")
+
+        _path = modelpath.split(".")
+        _sub_path, _class = ".".join(_path[:-1]), _path[-1]
+
+        _module = __import__(_sub_path, globals(), locals(), [_class], 0)
+        # pypy:
+        if _module is None:  # pragma: no cover
+            raise ImportError(modelpath)
+
+        return getattr(_module, _class)
+
+    def read(self, parent):
+        self.check_model(parent)
+        return super().read(parent)
+
+    def write(self, parent, value):
+        self.check_model(parent)
+        return super().write(parent, value)
+
+    @property
+    def model(self):
+        self.check_model()
+        return self._model
+
+    def check_model(self, parent=None):
+        if isinstance(self._model, (list, tuple)):
+            self._model = type(self._model)(
+                self._check_model(m, parent) for m in self._model
+            )
+        else:
+            self._model = self._check_model(self._model, parent)
+
+    def _check_model(self, model, parent):
+        if isinstance(model, (str, bytes)):
+            try:
+                return ModelLoader.cache[model]
+            except KeyError:
+                try:
+                    ModelLoader.cache[model] = self.load_model(model)
+                except ValueError:
+                    raise ImportError
+
+                return ModelLoader.cache[model]
+
+        elif parent and not isinstance(model, type):
+            return type(
+                "{}.{}".format(parent.__class__.__name__, self.key(parent)),
+                (model.__class__,),
+                model.attributes,
+            )
+
+        else:
+            return model
 
 
 class ModelAttribute(ModelLoader, BaseAttribute):
