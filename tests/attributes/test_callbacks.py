33fafad704bbde724eb8eb86d7b21e3dd3c6d2fc
Éloi Rivard <eloi.rivard@aquilenet.fr>
2021-05-27 12:03:33 +0200

    Additional tests

--- tests/attributes/test_callbacks.py
+++ tests/attributes/test_callbacks.py
@@ -100,3 +100,47 @@ def test_on_deletion(sheraf_connection):
     assert not m.deleted
     del m.foo
     assert m.deleted
+
+
+class Farm(tests.IntAutoModel):
+    owner = sheraf.ReverseModelAttribute("Cowboy", "farm")
+    size = sheraf.IntegerAttribute()
+
+    @size.on_edition
+    def update_size(self, new, old):
+        attr = self.owner.attributes["farm"]
+        old_values = self.owner.before_index_edition(attr)
+        yield
+        self.owner.after_index_edition(attr, old_values)
+
+
+class Cowboy(tests.IntAutoModel):
+    farm = sheraf.ModelAttribute("Farm").index(unique=True)
+
+    farm_size = sheraf.Index(farm)
+
+    @farm_size.search
+    def search_farm_size(self, size):
+        return size
+
+    @farm_size.values
+    def index_farm_size(self, farm):
+        return farm.size
+
+
+def test_back_indexation(sheraf_connection):
+    farm = Farm.create(size=100)
+    cowboy = Cowboy.create(farm=farm)
+
+    assert cowboy in Cowboy.search(farm_size=100)
+
+    farm.size = 90
+
+    assert cowboy not in Cowboy.search(farm_size=100)
+    assert cowboy in Cowboy.search(farm_size=90)
+
+    farm.size = 80
+
+    assert cowboy not in Cowboy.search(farm_size=100)
+    assert cowboy not in Cowboy.search(farm_size=90)
+    assert cowboy in Cowboy.search(farm_size=80)
